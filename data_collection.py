6d21913a5501000390e5a0e3cdede06bf9741293
nsworen <nsworen@gmail.com>
2020-04-26 23:56:07 -0400

    uploaded scraper and run code

---
+++ data_collection.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 26 22:28:18 2020
+
+@author: Nik
+"""
+
+
+import glassdoor_scraper as gs 
+import pandas as pd
+
+path = "C:/Users/nswor/OneDrive/Documents/ds_salary_proj/chromedriver"
+
+df = gs.get_jobs('data scientist', 15, 'False', path, 15)
+
+df
